d194b83182bcf5cec43f59bb160811b9211ab220
AKP-13 <alexpeirson@hotmail.com>
2022-12-13 22:00:28 +0000

    Spacing. Change title

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title "Sweepstake Generator"', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/sweepstake generator/i);
+    expect(linkElement).toBeInTheDocument();
 });
